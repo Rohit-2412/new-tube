d8714a16317891b4655beae71f601c16fa4776fc
Rohit <rk212300@outlook.com>
2025-02-16 01:20:05 +0530

    added tRPC and initial setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Page = () => {
-  return <div>i will load videos in future</div>;
+const HomePage = async () => {
+  void trpc.hello.prefetch({ text: "Rohit" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Error...</div>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 };
 
-export default Page;
+export default HomePage;
