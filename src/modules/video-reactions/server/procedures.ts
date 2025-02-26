c1c3c58f65fa6ce9baa0ab037e5954d423208772
Rohit <rk212300@outlook.com>
2025-02-26 17:48:08 +0530

    added video reactions

---
+++ src/modules/video-reactions/server/procedures.ts
@@ -0,0 +1,106 @@
+import { and, eq } from "drizzle-orm";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
+import db from "@/db";
+import { videoReactions } from "@/db/schema";
+import { z } from "zod";
+
+export const videoReactionsRouter = createTRPCRouter({
+  like: protectedProcedure
+    .input(z.object({ videoId: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { videoId } = input;
+
+      const [existingVideoReactionLike] = await db
+        .select()
+        .from(videoReactions)
+        .where(
+          and(
+            eq(videoReactions.videoId, videoId),
+            eq(videoReactions.userId, userId),
+            eq(videoReactions.type, "like"),
+          ),
+        );
+
+      if (existingVideoReactionLike) {
+        const [deletedViewerReaction] = await db
+          .delete(videoReactions)
+          .where(
+            and(
+              eq(videoReactions.videoId, videoId),
+              eq(videoReactions.userId, userId),
+            ),
+          )
+          .returning();
+
+        return deletedViewerReaction;
+      }
+
+      const [createdVideoReaction] = await db
+        .insert(videoReactions)
+        .values({
+          videoId,
+          userId,
+          type: "like",
+        })
+        .onConflictDoUpdate({
+          target: [videoReactions.userId, videoReactions.videoId],
+          set: {
+            type: "like",
+          },
+        })
+        .returning();
+
+      return createdVideoReaction;
+    }),
+
+  dislike: protectedProcedure
+    .input(z.object({ videoId: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { videoId } = input;
+
+      const [existingVideoReactionDislike] = await db
+        .select()
+        .from(videoReactions)
+        .where(
+          and(
+            eq(videoReactions.videoId, videoId),
+            eq(videoReactions.userId, userId),
+            eq(videoReactions.type, "dislike"),
+          ),
+        );
+
+      if (existingVideoReactionDislike) {
+        const [deletedViewerReaction] = await db
+          .delete(videoReactions)
+          .where(
+            and(
+              eq(videoReactions.videoId, videoId),
+              eq(videoReactions.userId, userId),
+            ),
+          )
+          .returning();
+
+        return deletedViewerReaction;
+      }
+
+      const [createdVideoReaction] = await db
+        .insert(videoReactions)
+        .values({
+          videoId,
+          userId,
+          type: "dislike",
+        })
+        .onConflictDoUpdate({
+          target: [videoReactions.userId, videoReactions.videoId],
+          set: {
+            type: "dislike",
+          },
+        })
+        .returning();
+
+      return createdVideoReaction;
+    }),
+});
--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -2,30 +2,84 @@ import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { VideoGetOneOutput } from "../../types";
 import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { trpc } from "@/trpc/client";
+import { useClerk } from "@clerk/nextjs";
 
-export const VideoReactions = () => {
-  const viewerReaction: "like" | "dislike" = "like";
+interface VideoReactionsProps {
+  videoId: string;
+  likes: number;
+  dislikes: number;
+  viewerReaction: VideoGetOneOutput["viewerReaction"];
+}
+
+export const VideoReactions = ({
+  videoId,
+  likes,
+  dislikes,
+  viewerReaction,
+}: VideoReactionsProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const like = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+
+      // todo: invalidate liked playlist
+    },
+    onError: (error) => {
+      toast.error("You must be signed in to like a video");
+
+      if (error.data?.code == "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const dislike = trpc.videoReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+    onError: (error) => {
+      toast.error("You must be signed in to like a video");
+
+      if (error.data?.code == "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
   return (
     <div className="flex items-center flex-none">
       <Button
         className="rounded-l-full rounded-r-none gap-2 pr-4"
         variant={"secondary"}
+        onClick={() => {
+          like.mutate({ videoId });
+        }}
+        disabled={like.isPending || dislike.isPending}
       >
         <ThumbsUpIcon
           className={cn("size-5", viewerReaction === "like" && "fill-black")}
         />
-        {1}
+        {likes}
       </Button>
       <Separator orientation="vertical" className="h-7" />
       <Button
         className="rounded-r-full rounded-l-none gap-2 pl-4"
         variant={"secondary"}
+        onClick={() => {
+          dislike.mutate({ videoId });
+        }}
+        disabled={like.isPending || dislike.isPending}
       >
         <ThumbsDownIcon
-          className={cn("size-5", viewerReaction !== "like" && "fill-black")}
+          className={cn("size-5", viewerReaction === "dislike" && "fill-black")}
         />
-        {1}
+        {dislikes}
       </Button>
     </div>
   );
